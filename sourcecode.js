cd30083058bd466d9ffd208f793ea54ccc78ea19
Usuario <superjd10@superjd10.com>
2016-09-23 16:47:34 -0500

    Soon and required source code

---
+++ sourcecode.js
@@ -0,0 +1,14 @@
+$(function() {
+	var html = $("html").html();
+	$("<pre />", {
+		"html":   '&lt;!DOCTYPE html>\n&lt;html>\n' +
+				$("html")
+					.html()
+					.replace(/[<>]/g, function(m) { return {'<':'&lt;','>':'&gt;'}[m]})
+					.replace(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi,'<a href="$1">$1</a>') +
+				'\n&lt;/html>',
+				"class": "prettyprint"
+	}).appendTo("#source-code");
+
+	prettyPrint();
+});
