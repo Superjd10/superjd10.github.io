739ab367d1f6cddeb31aadba53df3ef65272a6ac
Usuario <superjd10@superjd10.com>
2016-09-23 20:13:39 -0500

    Thanks CSS Tricks

--- sourcecode.js
+++ sourcecode.js
@@ -6,7 +6,7 @@ $(function() {
 					.html()
 					.replace(/[<>]/g, function(m) { return {'<':'&lt;','>':'&gt;'}[m]})
 					.replace(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi,'<a href="$1">$1</a>') +
-				'\n&lt;/html>',
+				'\n&lt;/html> \n Live view-source thanks to <a href="https://css-tricks.com" title="CSS Tricks">CSS Tricks</a>',
 				"class": "prettyprint"
 	}).appendTo("#source-code");
 
